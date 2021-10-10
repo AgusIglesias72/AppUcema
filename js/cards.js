5ad41b6ce1a893feb13a6a7ecf618274a7ddd085
AgusIglesias72 <AgusIglesias72@gmail.com>
2021-10-10 20:10:27 -0300

    Probando responsive

---
+++ js/cards.js
@@ -0,0 +1,13 @@
+let stockTotal = []
+
+// STOCK BASES
+stockTotal.push(
+    {nombre: "Universidad del CEMA", Mail:"", WebSite:"" , linkedIn: "https://www.google.com/", descripción: "Breve Descripción del Producto para mostrar al hacer click", stock: 10, img: "./img/Logos/LogoUcema.jpg"},
+    {nombre: "Universidad del CEMA", Mail:"", WebSite:"" , linkedIn: "https://www.google.com/", descripción: "Breve Descripción del Producto para mostrar al hacer click", stock: 10, img: "./img/Logos/LogoUcema.jpg"},
+    {nombre: "Universidad del CEMA", Mail:"", WebSite:"" , linkedIn: "https://www.google.com/", descripción: "Breve Descripción del Producto para mostrar al hacer click", stock: 10, img: "./img/Logos/LogoUcema.jpg"},
+    {nombre: "Universidad del CEMA", Mail:"", WebSite:"" , linkedIn: "https://www.google.com/", descripción: "Breve Descripción del Producto para mostrar al hacer click", stock: 10, img: "./img/Logos/LogoUcema.jpg"},
+    {nombre: "Universidad del CEMA", Mail:"", WebSite:"" , linkedIn: "https://www.google.com/", descripción: "Breve Descripción del Producto para mostrar al hacer click", stock: 10, img: "./img/Logos/LogoUcema.jpg"},
+    {nombre: "Universidad del CEMA", Mail:"", WebSite:"" , linkedIn: "https://www.google.com/", descripción: "Breve Descripción del Producto para mostrar al hacer click", stock: 10, img: "./img/Logos/LogoUcema.jpg"},
+    {nombre: "Universidad del CEMA", Mail:"", WebSite:"" , linkedIn: "https://www.google.com/", descripción: "Breve Descripción del Producto para mostrar al hacer click", stock: 10, img: "./img/Logos/LogoUcema.jpg"},
+    {nombre: "Universidad del CEMA", Mail:"", WebSite:"" , linkedIn: "https://www.google.com/", descripción: "Breve Descripción del Producto para mostrar al hacer click", stock: 10, img: "./img/Logos/LogoUcema.jpg"},
+)
--- js/index.js
+++ js/index.js
@@ -1,21 +1,52 @@
+const template = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
+const card = document.getElementById('cardContainer')
+
+
+const showCard = () => {
+  stockTotal.forEach(elemento =>{
+    template.getElementById('image').setAttribute('src', elemento.img)
+    template.getElementById('body-title').textContent = elemento.nombre
+    
+    template.getElementById('linkedIn').setAttribute('href', elemento.linkedIn)
+    template.getElementById('E-Mail').setAttribute('href', elemento.Mail)
+    template.getElementById('WebSite').setAttribute('href', elemento.WebSite)
+
+    // if (elemento.linkedIn == ""){
+    //   template.getElementById('linkedIn').innerHTML = ""
+    // }
+    // if (elemento.Mail == ""){
+    //   template.getElementById('E-Mail').innerHTML = ""
+    // }
+    // if (elemento.WebSite == ""){
+    //   template.getElementById('WebSite').innerHTML = ""
+    // }
+  
+    const clone = template.cloneNode(true)
+    fragment.appendChild(clone)
+  }) 
+  card.insertBefore(fragment, card.children[1])
+}
+showCard()
+
+
 $num = $('.ui-card').length;
 $even = $num / 2;
 $odd = ($num + 1) / 2;
- 
-if ($num % 2 == 0) {
-  $('.ui-card:nth-child(' + $even + ')').addClass('active');
-  $('.ui-card:nth-child(' + $even + ')').prev().addClass('prev');
-  $('.ui-card:nth-child(' + $even + ')').next().addClass('next');
-} else {
-  $('.ui-card:nth-child(' + $odd + ')').addClass('active');
-  $('.ui-card:nth-child(' + $odd + ')').prev().addClass('prev');
-  $('.ui-card:nth-child(' + $odd + ')').next().addClass('next');
-}
- 
-$('.ui-card').click(function() {
-  $slide = $('.active').width();
-  console.log($('.active').position().left);
- 
+
+  if ($num % 2 == 0) {
+    $('.ui-card:nth-child(' + $even + ')').addClass('active');
+    $('.ui-card:nth-child(' + $even + ')').prev().addClass('prev');
+    $('.ui-card:nth-child(' + $even + ')').next().addClass('next');
+
+  } else {
+    $('.ui-card:nth-child(' + $odd + ')').addClass('active');
+    $('.ui-card:nth-child(' + $odd + ')').prev().addClass('prev');
+    $('.ui-card:nth-child(' + $odd + ')').next().addClass('next');
+  }
+
+$('.ui-card').click(function(e) {
+  $slide = 0; 
   if ($(this).hasClass('next')) {
     $('.container').stop(false, true).animate({left: '-=' + $slide});
   } else if ($(this).hasClass('prev')) {
@@ -23,12 +54,13 @@ $('.ui-card').click(function() {
   }
  
   $(this).removeClass('prev next');
-  $(this).siblings().removeClass('prev active next');
+  $(this).siblings().removeClass('prevprev prev active next nextnext');
  
   $(this).addClass('active');
   $(this).prev().addClass('prev');
   $(this).next().addClass('next');
-});
+  }
+);
  
  
 // Keyboard nav
